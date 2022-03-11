467424d9a5dacc3e8a9da1a70633666c7f58079f
Luis Ball <lball@imgix.com>
2022-03-11 09:57:20 -0800

    build: change configs to mjs and create storybook config files

---
+++ web-dev-server.storybook.mjs
@@ -0,0 +1,28 @@
+/**
+ * Storybook Web Dev Server
+ *
+ * Provides a local env for running storybook with modern ESM JavaScript
+ */
+
+import image from '@rollup/plugin-image';
+import {esbuildPlugin} from '@web/dev-server-esbuild';
+import {rollupAdapter} from '@web/dev-server-rollup';
+import {storybookPlugin} from '@web/dev-server-storybook';
+import styles from 'rollup-plugin-styles';
+import baseConfig from './web-dev-server.config.mjs';
+
+export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
+  ...baseConfig,
+  open: './',
+  mimeTypes: {
+    '**/*.json': 'js',
+    '**/*.css': 'js',
+  },
+  plugins: [
+    ...baseConfig.plugins,
+    rollupAdapter(styles()),
+    rollupAdapter(image()),
+    storybookPlugin({type: 'web-components', configDir: '.storybook'}),
+    esbuildPlugin({ts: true, json: true, target: 'esnext'}),
+  ],
+});
