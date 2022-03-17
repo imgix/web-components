f9df375cf5faeacd731dfc6cfcc0154d6cf99278
sherwinski <sherwin@imgix.com>
2022-03-16 23:46:45 -0700

    feat: add supported video parameters

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,8 @@
 export const STATUS_CODE_QUERY_ERROR = 400;
 export const STATUS_CODE_NOT_FOUND = 404;
 export const STATUS_CODE_REDIRECT_OK = 302;
+
+// Supported Parameters
+export const VIDEO_PARAMETERS = {
+  fm: 'hls'
+}
