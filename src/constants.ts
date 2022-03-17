25ba0b6edb619608c367abb03147f370e1bcba31
sherwinski <sherwin@imgix.com>
2022-03-17 13:47:20 -0700

    feat: create interface for video component

--- src/constants.ts
+++ src/constants.ts
@@ -7,3 +7,14 @@ export const STATUS_CODE_REDIRECT_OK = 302;
 export const VIDEO_PARAMETERS = {
   fm: 'hls'
 }
+
+/*
+ * src: URL of the .video file to be rendered. Can be passed a relative path, in accompaniment with the `domain` prop, or an absolute URL.
+ * domain: If the src URL is relative, it will be appended to the domain.
+ * dataSetup: video.js modifiers, which will be passed on to video.js.
+ */
+export interface VideoProps {
+  src: string;
+  domain?: string;
+  dataSetup?: string;
+}
