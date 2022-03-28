8dc66bac03202a8ce7f2ac225ff5a6ba27807111
Luis Ball <lball@imgix.com>
2022-03-28 16:42:54 -0700

    feat: remove shadow dom and import videojs css
    This commit removes the `shadow-dom` from the generate node-tree. This
    allows us to import then video.js styles from the node_modules without
    need to neither 1) write our own, WC scoped styles for each default
    style or 2) find a way to scrop that imported stylesheet to the WC.

--- src/ix-video.ts
+++ src/ix-video.ts
@@ -1,22 +1,14 @@
-import {css, html, LitElement} from 'lit';
+import {html, LitElement} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
-import {ref, createRef} from 'lit/directives/ref.js';
-
+import {createRef, ref} from 'lit/directives/ref.js';
 import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
 
 /**
- *
- * @fires ix-video-clicked - Indicates that the video has been clicked on
  * @csspart video - The video element
  */
 @customElement('ix-video')
 export class IxVideo extends LitElement {
-  static override styles = css`
-    :host {
-      display: block;
-    }
-  `;
-
   videoRef = createRef<HTMLVideoElement>();
 
   /**
@@ -24,14 +16,23 @@ export class IxVideo extends LitElement {
    */
   @property({type: String})
   videoSrc = '';
-
   override render() {
     return html`
-      <div>
-        <h1>videoSrc: ${this.videoSrc}</h1>
-        <video src=${this.videoSrc} controls id="ix-video-player" ${ref(this.videoRef)} part="video">
-        </video>
-      </div>
+      <style>
+        #ix-video-player {
+          width: 450px;
+          height: 250px;
+        }
+      </style>
+      <video
+        ${ref(this.videoRef)}
+        autoplay
+        class="video-js vjs-default-skin"
+        controls
+        id="ix-video-player"
+        part="video"
+        src=${this.videoSrc}
+      ></video>
     `;
   }
 
@@ -40,6 +41,17 @@ export class IxVideo extends LitElement {
       videojs.log('Your player is ready!');
       console.log(this);
     });
+  }
+
+  /**
+   * This is a hack to disable Shadow DOM in lit-element. Removes the shadowRoot
+   * and renders the elements as children of the host element.
+   *
+   * This also impedes our ability to use `css` and `cssPart` in lit-element.
+   * Styling is instead done in the style tag.
+   */
+  protected override createRenderRoot() {
+    return this;
   }
 }
 
