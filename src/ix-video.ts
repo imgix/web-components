57c9dd06c8e52b333d934d8578a730be311952ac
Luis Ball <lball@imgix.com>
2022-03-30 15:43:22 -0700

    feat: add data-setup attribute
    Before this commit:
    - w/h defaulted to `'100%'` rather than `''`. This made it difficult to
      only give the video element those attributes when neither the w/h
    attributes where set _and_ the data-setup attribute w/h were not set.
    - data-setup attribute could not be set
    - controls attribute defaulted to true
    
    After this commit:
    - width/height default to `''` if not set
    - if w/h not set, `<video>` element is given a w/h of `'100%'`
    - controls can be turned on/off
    - `dataStup` attribute can be set exactly as it is in video.js
    - if `dataSetup` doesn't have a source, w, h, or controls property that
      property is set to the value of the corresponding attribute.

--- src/ix-video.ts
+++ src/ix-video.ts
@@ -1,8 +1,11 @@
 import {html, LitElement} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import {createRef, ref} from 'lit/directives/ref.js';
-import videojs from 'video.js';
+import {styleMap} from 'lit/directives/style-map.js';
+import videojs, {VideoJsPlayerOptions} from 'video.js';
 import 'video.js/dist/video-js.css';
+import {convertOptionsStringToObject} from './converters';
+import {buildOptionsObj} from './helpers';
 
 /**
  * @csspart video - The video element
@@ -23,31 +26,60 @@ export class IxVideo extends LitElement {
    * Video player width
    */
   @property({type: String})
-  width = '100%';
+  width = '';
 
   /**
    * Video player height
    */
   @property({type: String})
-  height = '100%';
+  height = '';
+
+  /**
+   * Show/hide the video controls
+   * @default true
+   */
+  @property({type: Boolean, attribute: 'controls'})
+  controls = false;
+
+  /**
+   * Video.js dataSetup options json string. Converts to a VideoJsPlayerOptions
+   * object. If not set, defaults to {}. If invalid json, defaults to {}.
+   * @see https://docs.videojs.com/tutorial-options.html
+   */
+  @property({
+    type: String,
+    converter: (value) => convertOptionsStringToObject(value),
+  })
+  dataSetup = {} as VideoJsPlayerOptions;
 
   override render() {
+    // if no w/h attr set, we default to 100% of parent dimensions.
+    const noWidthOrHeight = !this.width.length && !this.height.length;
     return html`
       <video
         ${ref(this.videoRef)}
+        style=${noWidthOrHeight &&
+        styleMap({
+          width: '100%',
+          height: '100%',
+        })}
         class="video-js vjs-default-skin"
-        controls
         id="ix-video-player"
         part="video"
-      >
-        <source src="${this.source}" type="application/x-mpegURL" />
-      </video>
+      ></video>
     `;
   }
 
   override firstUpdated(): void {
-    videojs(this.videoRef?.value as HTMLVideoElement, {}, () => {
-      videojs.log('Your player is ready!');
+    const player = this.videoRef?.value as HTMLVideoElement;
+    const options = buildOptionsObj(this.dataSetup, {
+      source: this.source,
+      controls: this.controls,
+      width: this.width,
+      height: this.height,
+    });
+    videojs(player, options, () => {
+      videojs.log('player ready');
     });
   }
 
