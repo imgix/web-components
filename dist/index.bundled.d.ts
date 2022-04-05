87e86b6d07adf4192fd92f252f0847c528750121
Luis Ball <lball@imgix.com>
2022-04-05 16:23:20 -0700

    build: commit new prod build files

--- dist/index.bundled.d.ts
+++ dist/index.bundled.d.ts
@@ -18,8 +18,27 @@ declare class IxVideo extends LitElement {
      * Video player height
      */
     height: string;
+    /**
+     * Show/hide the video controls
+     * @default true
+     */
+    controls: boolean;
+    /**
+     * Video.js dataSetup options json string. Converts to a VideoJsPlayerOptions
+     * object. If not set, defaults to {}. If invalid json, defaults to {}.
+     * @see https://docs.videojs.com/tutorial-options.html
+     */
+    dataSetup: string;
     render(): import("lit-html").TemplateResult<1>;
     firstUpdated(): void;
+    /**
+     * This is a hack to disable Shadow DOM in lit-element. Removes the shadowRoot
+     * and renders the elements as children of the host element.
+     *
+     * This also impedes our ability to use `css` and `cssPart` in lit-element.
+     * Styling is instead done in the style tag.
+     */
+    protected createRenderRoot(): this;
 }
 declare global {
     interface HTMLElementTagNameMap {
