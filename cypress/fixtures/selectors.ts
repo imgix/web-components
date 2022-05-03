66d0b35c35df45010afb18c471167d2de2c581ca
Luis H. Ball Jr <luqven@gmail.com>
2022-05-03 15:17:25 -0700

    test: ix-video react integration tests (#37)
    - feat: react to props change (#38)
    - feat: make video sizing responsive by default (#39)

--- cypress/fixtures/selectors.ts
+++ cypress/fixtures/selectors.ts
@@ -11,3 +11,4 @@ export const PLAYER_WITH_WIDTH_AND_HEIGHT = `[data-test-id=ix-video-with-w-and-h
 export const PLAYER_WITH_CONTAINER_VIDEO_ATT = `[data-test-id=ix-video-with-video-attributes]`;
 export const PLAYER_WITH_CONTAINER_MP4 = `[data-test-id=ix-video-mp4-player]`;
 export const PLAYER_WITH_DATA_SETUP = `[data-test-id=ix-video-with-data-setup]`;
+export const REACT_PLAYER = `[data-test-id=react-player]`;
